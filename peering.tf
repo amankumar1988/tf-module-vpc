e80cfa39e227b662ba895bb2b88551ae88bc0085
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-21 22:44:34 +0530

    Auto Push

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peer" {
   #peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = "vpc-0ace177c971986a50"
 
 }
