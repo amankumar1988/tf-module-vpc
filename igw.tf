b923f7b0fe043b708b22beb8ddea7f89da9522c2
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-24 21:59:06 +0530

    Auto Push

--- igw.tf
+++ igw.tf
@@ -29,5 +29,5 @@ resource "aws_nat_gateway" "ngw" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_eip.ngw-ip]
 }
