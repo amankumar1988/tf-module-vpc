9b99b271e05862fe9c179e65721bf1c2b2207bc3
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-24 22:00:23 +0530

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -26,21 +26,21 @@ resource "aws_route_table_association" "public-rt-association" {
 }
 
 
-# # Create public route tables
-# resource "aws_route_table" "private-rt" {
-#   vpc_id = aws_vpc.main.id
+# Create public route tables
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_internet_gateway.ngw.id
+  }
 
-#   route {
-#     cidr_block = var.DEFAULT_VPC_CIDR
-#     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-#   }
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
 
-#   tags = {
-#     Name = "roboshop-${var.ENV}-privatec-rt"
-#   }
-# }
+  tags = {
+    Name = "roboshop-${var.ENV}-private-rt"
+  }
+}
