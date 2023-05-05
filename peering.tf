c42185644ca08f141e22f3e0e3ec748a1d48398b
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 12:56:54 +0530

    Auto Push

--- peering.tf
+++ peering.tf
@@ -1,7 +1,5 @@
 resource "aws_vpc_peering_connection" "peer" {
-  #peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_ID
-  auto_accept = true
-
+  auto_accept   = true                  # This options is valid if both the VPC's are in the same account and in the same region.
 }
--- route-table.tf
+++ route-table.tf
@@ -1,4 +1,4 @@
-# Create public route tables
+# Creates Public route Table
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
 
@@ -8,8 +8,8 @@ resource "aws_route_table" "public-rt" {
   }
 
   route {
-    cidr_block = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
@@ -17,27 +17,26 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-# Attach publit rt with subnets
-
+# Attaches the public rt to the public subnets
 resource "aws_route_table_association" "public-rt-association" {
-  count =  length (aws_subnet.public_subnet.*.id)
+  count          = length(aws_subnet.public_subnet.*.id)
+
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public-rt.id
 }
 
-
-# Create Private route tables
+# # Creates Private route Table
 resource "aws_route_table" "private-rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id           = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
-    cidr_block = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
@@ -45,16 +44,16 @@ resource "aws_route_table" "private-rt" {
   }
 }
 
-# Attach public rt with subnets
-
+# Attaches the private rt to the private subnets
 resource "aws_route_table_association" "private-rt-association" {
-  count =  length (aws_subnet.private_subnet.*.id)
+  count          = length(aws_subnet.private_subnet.*.id)
+
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private-rt.id
 }
 
-# Adding the peering route entry in default route-table to access robot NW
 
+# Adding the peering route entry insde the default route-table to access robot network
 resource "aws_route" "def-vpc-robot-vpc-root" {
   route_table_id            = var.DEFAULT_VPC_RT
   destination_cidr_block    = var.VPC_CIDR
